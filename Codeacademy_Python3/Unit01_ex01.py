25beee7f943935ce3c51e5a81419ab9179bd06a2
carlinhocr <carlos.rienzi@gmail.com>
2021-07-20 16:51:04 -0300

    2

---
+++ Codeacademy_Python3/Unit01_ex01.py
@@ -0,0 +1,12 @@
+def main():
+
+    fraction = 1/3
+    print(format(fraction,'.14g'))
+    print(format(fraction,'.2f'))
+
+    fraction = 1/10
+    fraction = 51.6
+    print(format(fraction,'.23g'))
+    print(format(fraction,'.2f'))
+if __name__ == "__main__":
+    main()
