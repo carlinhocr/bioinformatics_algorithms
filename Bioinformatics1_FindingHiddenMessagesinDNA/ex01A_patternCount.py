c4cf634b33f5a3eefc5ce4414406ed0b0ad96611
carlinhocr <carlos.rienzi@gmail.com>
2021-09-06 17:40:04 -0300

    2

--- Bioinformatics1_FindingHiddenMessagesinDNA/ex01A_patternCount.py
+++ Bioinformatics1_FindingHiddenMessagesinDNA/ex01A_patternCount.py
@@ -13,6 +13,25 @@ class Bioinformatics(object):
         print("It repeats",count)
         return count
 
+    def FrequentWords(self,Text, k):
+        words = []
+        freq = self.FrequencyMap(Text, k)
+        m = max(freq.values())
+        for key in freq:
+            if freq[key] == m:
+                words.append(key)
+        return words
+
+    def FrequencyMap(self,Text, k):
+        freq = {}
+        n = len(Text)
+        for i in range(n - k + 1):
+            Pattern = Text[i:i + k]
+            freq[Pattern] = 0
+        for i in range(n - k + 1):
+            Pattern = Text[i:i + k]
+            freq[Pattern] += 1
+        return freq
 
 def main():
 
