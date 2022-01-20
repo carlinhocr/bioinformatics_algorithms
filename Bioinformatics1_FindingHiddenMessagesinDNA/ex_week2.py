4de69580fc26bbcbab41b8679827683dbf718331
carlinhocr <carlos.rienzi@gmail.com>
2022-01-20 15:31:33 -0300

    2

---
+++ Bioinformatics1_FindingHiddenMessagesinDNA/ex_week2.py
@@ -0,0 +1,15 @@
+class Bioinformatics(object):
+    def __init__(self):
+        pass
+
+
+#-------------------------------------------------------------------------------
+
+
+def main():
+
+    bio = Bioinformatics()
+
+
+if __name__ == "__main__":
+    main()
