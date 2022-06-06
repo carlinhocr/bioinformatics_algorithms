6019b0bd2ee67cd03b12d677312116ae350b01b3
carlinhocr <carlos.rienzi@gmail.com>
2022-06-06 12:19:49 -0300

    terminando la week 4

--- Bioinformatics1_FindingHiddenMessagesinDNA/ex_week4.py
+++ Bioinformatics1_FindingHiddenMessagesinDNA/ex_week4.py
@@ -162,30 +162,30 @@ class Bioinformatics(object):
 
 # ==============================
 
-    # def score(self,motifs):
-    #     consensusString = self.consensus(motifs)
-    #     countMatrix = self.count(motifs)
-    #     score = 0
-    #     k = len(motifs[0])
-    #     for symbol in "ACGT":
-    #         for j in range (k):
-    #             if symbol != consensusString[j]:
-    #                 score += countMatrix[symbol][j]
-    #     return score
-    #
-    # def count(self, motifs):
-    #     k = len(motifs[0])
-    #     count = {}
-    #     for symbol in "ACGT":
-    #         count[symbol] = []
-    #         for j in range(k):
-    #             count[symbol].append(0)
-    #     t = len(motifs)
-    #     for i in range(t):
-    #         for j in range(k):
-    #             symbol = motifs[i][j]
-    #             count[symbol][j] += 1
-    #     return count
+    def score(self,motifs):
+        consensusString = self.consensus(motifs)
+        countMatrix = self.count(motifs)
+        score = 0
+        k = len(motifs[0])
+        for symbol in "ACGT":
+            for j in range (k):
+                if symbol != consensusString[j]:
+                    score += countMatrix[symbol][j]
+        return score
+
+    def count(self, motifs):
+        k = len(motifs[0])
+        count = {}
+        for symbol in "ACGT":
+            count[symbol] = []
+            for j in range(k):
+                count[symbol].append(0)
+        t = len(motifs)
+        for i in range(t):
+            for j in range(k):
+                symbol = motifs[i][j]
+                count[symbol][j] += 1
+        return count
     #
     # def greedyMotifSearch(self,dna,k,t):
     #     bestMotifs = []
@@ -225,67 +225,68 @@ class Bioinformatics(object):
     #     m = self.motif(profile, dna, k)
     #     print (m)
     #
-    # def normalize(self,probabilities):
-    #     sum = 0
-    #     for element in probabilities.values():
-    #         sum += element
-    #     for element2 in probabilities.keys():
-    #         probabilities[element2]=probabilities[element2]/sum
-    #     return probabilities
-    #
-    # def weightedDie(self,probabilities):
-    #     kmer = ''
-    #     number = random.uniform(0,1)
-    #     acum = 0
-    #     for element in probabilities.keys():
-    #         if number >= acum and number < acum + probabilities[element]:
-    #             kmer = element
-    #         acum += probabilities[element]
-    #     return kmer
-    #
-    # def profileGeneratedString(self, Text, profile, k):
-    #     n = len(Text)
-    #     probabilities = {}
-    #     probabilities_normalized = {}
-    #     probabilities_weighted = {}
-    #     for i in range(0,n-k+1):
-    #         kmer = Text[i:i+k]
-    #         probabilities[kmer] = self.kmerProb(kmer,profile)
-    #     probabilities_normalized = self.normalize(probabilities)
-    #     string_probabilities_weighted = self.weightedDie(probabilities_normalized)
-    #     return string_probabilities_weighted
-    #
-    # def cloning(self, li1):
-    #     li_copy = li1[:]
-    #     return li_copy
-    #
-    # def gibbsSampler(self,dna, k, t, N):
-    #     bestMotifs = []
-    #     motifs = self.randomMotifs(dna,k,t)
-    #     bestMotifs = motifs
-    #     for j in range(1, N):
-    #         i = random.randint(0, t-1)
-    #         motifs_without_i= self.cloning(motifs)
-    #         motifs_selected = motifs[i]
-    #         motifs_without_i.pop(i)
-    #         profile = self.profileWithPseudocounts(motifs_without_i)
-    #         dna_i = dna[i]
-    #         kmer_profile_weighted = self.profileGeneratedString(dna_i,profile,k)
-    #         motifs.pop(i) # it is more clear to use an auxliary list motifs_without_i for debugging
-    #         motifs.insert(i, kmer_profile_weighted)
-    #         if self.score(motifs) < self.score(bestMotifs):
-    #             bestMotifs = motifs
-    #     return bestMotifs
-    #
-    # def gibbsSamplerRepeatXtimes(self,dna,k,t,n,x):
-    #     motifs = self.randomMotifs(dna,k,t)
-    #     bestMotifs = motifs
-    #     for i in range(0,x):
-    #         m = self.gibbsSampler(dna,k,t,n)
-    #         if self.score(m) < self.score(bestMotifs):
-    #             bestMotifs = m
-    #     return bestMotifs
-    #
+    def normalize(self,probabilities):
+        sum = 0
+        for element in probabilities.values():
+            sum += element
+        for element2 in probabilities.keys():
+            probabilities[element2]=probabilities[element2]/sum
+        return probabilities
+
+
+    def weightedDie (self, probabilities):
+        kmer = ''
+        number = random.uniform(0,1)
+        acum = 0
+        for element in probabilities.keys():
+            if number >= acum and number < acum + probabilities[element]:
+                kmer = element
+            acum += probabilities[element]
+        return kmer
+
+    def profileGeneratedString(self, Text, profile, k):
+        n = len(Text)
+        probabilities = {}
+        probabilities_normalized = {}
+        probabilities_weighted = {}
+        for i in range(0,n-k+1):
+            kmer = Text[i:i+k]
+            probabilities[kmer] = self.kmerProb(kmer,profile)
+        probabilities_normalized = self.normalize(probabilities)
+        string_probabilities_weighted = self.weightedDie(probabilities_normalized)
+        return string_probabilities_weighted
+
+    def cloning(self, li1):
+        li_copy = li1[:]
+        return li_copy
+
+    def gibbsSampler(self,dna, k, t, N):
+        bestMotifs = []
+        motifs = self.randomMotifs(dna,k,t)
+        bestMotifs = motifs
+        for j in range(1, N):
+            i = random.randint(0, t-1)
+            motifs_without_i= self.cloning(motifs)
+            motifs_selected = motifs[i]
+            motifs_without_i.pop(i)
+            profile = self.profileWithPseudocounts(motifs_without_i)
+            dna_i = dna[i]
+            kmer_profile_weighted = self.profileGeneratedString(dna_i,profile,k)
+            motifs.pop(i) # it is more clear to use an auxliary list motifs_without_i for debugging
+            motifs.insert(i, kmer_profile_weighted)
+            if self.score(motifs) < self.score(bestMotifs):
+                bestMotifs = motifs
+        return bestMotifs
+
+    def gibbsSamplerRepeatXtimes(self,dna,k,t,n,x):
+        motifs = self.randomMotifs(dna,k,t)
+        bestMotifs = motifs
+        for i in range(0,x):
+            m = self.gibbsSampler(dna,k,t,n)
+            if self.score(m) < self.score(bestMotifs):
+                bestMotifs = m
+        return bestMotifs
+
 
 def main():
 
@@ -304,8 +305,32 @@ def main():
     #     list = bio.randomizedMotifSearchNTimes(dna_sequences,k,t,n)
     #     print(*list) # separate the elements with one space only (using *
     # ---------------------------------------------------------------------------------------------------------------
-    print(bio.probabilityAllKmersFoundNStrings(numberOfKmers=10,kmersLenght=15,nucleotidesLengh=600))
-    print(bio.probabilityOfAtLeast2Found(numberOfKmers=10, kmersLenght=15, nucleotidesLengh=600))
-
+    # print(bio.probabilityAllKmersFoundNStrings(numberOfKmers=10,kmersLenght=15,nucleotidesLengh=600))
+    # print(bio.probabilityOfAtLeast2Found(numberOfKmers=10, kmersLenght=15, nucleotidesLengh=600))
+    # k = 8
+    # t = 5
+    # n = 100
+    # dna_sequences = ['CGCCCCTCTCGGGGGTGTTCAGTAACCGGCCA',
+    # 'GGGCGAGGTATGTGTAAGTGCCAAGGTGCCAG',
+    # 'TAGTACCGAGACCGAAAGAAGTATACAGGCGT',
+    # 'TAGATCAAGTTTCAGGTGCACGTCGGTGAACC',
+    # 'AATCCACCAGCTCCACGTGCAATGTTGGCCTA']
+    # list = bio.gibbsSampler(dna_sequences,k,t,n)
+    # print(*list)
+    filename = 'dataset_163_4.txt'
+    with open(filename, "r") as dataset:
+        data = []
+        for line in dataset:
+            data.append(line.strip())
+        k_string,t_string,n_string = data[0].split()
+        k = int(k_string)
+        t = int(t_string)
+        n = int(n_string)
+        dna_sequences = data[1].strip().split() # several motifs
+        print(dna_sequences,k,t,n)
+        random_starts = 20
+        lista = bio.gibbsSamplerRepeatXtimes(dna_sequences,k,t,n,random_starts)
+        print(*lista) # separate the elements with one space only using *
+    # ---------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     main()
